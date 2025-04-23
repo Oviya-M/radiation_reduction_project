437e729c3067793b797b1b5ed12bc766d4a9f1d0
Sophie Walters <smw59@uw.edu>
2025-04-23 13:45:21 -0700

    Restore setup.py and finalize publisher node

---
+++ rad_reduction_perception/launch/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'rad_reduction_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sophiewalters3',
+    maintainer_email='smw59@uw.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_subscriber = my_subscriber:main',
+            'publisher = publisher:main',
+        ],
+    },
+)
